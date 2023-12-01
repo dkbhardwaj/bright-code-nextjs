fe97681f5c56d6ffe3bdef82bbbdc29af3f0d26d
Ganesh <ganesh@webenertia.com>
2023-12-01 14:19:20 +0530

    updated favicon and title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import type { ReactElement, ReactNode } from "react";
+import { Metadata } from "next";
+import Head from "next/head";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
 import Layout from "../components/layout";
 import "../app/globals.css";
 import "../styles/global.css";
 import "../styles/buttons.css";
+import { title } from "process";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -17,12 +20,17 @@ type AppPropsWithLayout = AppProps & {
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => page);
-
+  const defaultTitle = "Bright Code";
   return (
-    <Layout>
-      <main>
-        <Component {...pageProps} />
-      </main>
-    </Layout>
+    <>
+      <Head>
+        <title>{defaultTitle}</title>
+      </Head>
+      <Layout>
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </Layout>
+    </>
   );
 }
