3a646d556b43919215230a55f0d499fd8c775815
Navneet <navneet@webenertia.com>
2023-12-02 19:18:09 +0530

    hero banner content update

--- components/banner.tsx
+++ components/banner.tsx
@@ -7,20 +7,20 @@ const HeroBanner: React.FC = () => {
         <div className="w-mainRow -ml-2.5 flex items-center">
           <div className="w-halfWidth mx-2.5">
             <div className="content">
-              <h1>Build stunning websites with NextJs UI Kit</h1>
-              <h4 className={`max-w-[350px] mt-2`}>
+              <h1>Empowering Your Digital Success</h1>
+              {/* <h4 className={`max-w-[350px] mt-2`}>
                 Free Reactstrap UI Kit with Lots of Ready to Use Sections
-              </h4>
+              </h4> */}
               <div className="btnWrap mt-8 inline-block">
                 <a href="#" className="blue-btn">
                   Download Free
                 </a>
               </div>
-              <div className="btnWrap mt-8 ml-5 inline-block">
+              {/* <div className="btnWrap mt-8 ml-5 inline-block">
                 <a href="#" className="transparent-btn">
                   Download Free
                 </a>
-              </div>
+              </div> */}
             </div>
           </div>
           <div className="w-halfWidth mx-2.5">
