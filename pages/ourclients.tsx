b34bba3235ced58c09b197c1e3876b712116e7a7
Navneet <navneet@webenertia.com>
2023-12-28 15:21:49 +0530

    Recaptcha key code remove

--- pages/ourclients.tsx
+++ pages/ourclients.tsx
@@ -370,11 +370,11 @@ const Ourclient: React.FC = () => {
                   errors={state.errors}
                 />
 
-                <ReCAPTCHA
+                {/* <ReCAPTCHA
                   sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY as string}
                   onChange={setcaptcha}
                   className="mb-5 mx-[10px]  md:mx-0"
-                />
+                /> */}
                 <button
                   type="submit"
                   className="gradient-btn max-w-full  mx-[10px] mb-[22px] md:mx-0"
