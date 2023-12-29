220bb90ce28f9be223ba5a96c12c6657006b3f23
Bhawana <bhawana@webenertia.com>
2023-12-29 13:07:30 +0530

    header update

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -172,7 +172,7 @@ const Navigation: React.FC<NavigationProps> = () => {
             </ul> */}
             <div className={`header-menu ${ showMobileMenu ? 'lg:left-0' : 'lg:!-left-[calc(100vw+500px)]' } relative lg:fixed border-none lg:left-0 lg:top-0 lg:!w-[calc(100vw+500px)] lg:h-[100vh] lg:bg-[#00000066] lg:transition-all lg:duration-1000`}>
             <div className={`navbar-wrap relative lg:absolute lg:left-0 lg:top-0 lg:!w-[350px] lg:h-[100vh] lg:overflow-y-scroll lg:scroll-smooth z-10 lg:bg-white  lg:py-5 `}>
-            <div className={`${Style.logo_wrapper} hidden relative w-full py-5 px-3 mb-7 lg:!flex bg-darkGray flex-wrap items-center justify-between `}>
+            <div className={`${Style.logo_wrapper}  hidden relative w-full py-5 px-3 mb-7 lg:!flex bg-bgBluePurple flex-wrap items-center justify-between `}>
               <div className={`${Style.logo} relative max-w-[200px] max-h-[45px]`}>
             <Link
               href="/"
@@ -237,16 +237,16 @@ const Navigation: React.FC<NavigationProps> = () => {
                   Our clients
                 </Link>
               </li>
-                <Link
+              <Link
                   href="/contact"
-                  className={`${Style.btn} ml-[30px] gradient-btn border-btn lg:ml-0 lg:my-8 `}>
+                  className={`${Style.btn} ml-[30px] gradient-btn border-btn lg:!hidden lg:ml-0 lg:my-8 `}>
                   <span>Contact</span>
                 </Link>
-                {/* <Link
+                <Link
                   href="/contact"
                   className={`${Style.btn} gradient-btn !hidden lg:!inline-block my-8 !shadow-none`}>
                   <span>Contact</span>
-                </Link> */}
+                </Link>
             </ul>
             </div>
             </div>
