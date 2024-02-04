d6008a6084628cbc14ce96c35c562095cc7f327f
Stefan Iordan <stefan.iordan@gmail.com>
2024-02-04 13:00:48 +0200

    twitter and linkedin links in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -933,7 +933,7 @@ const Home: React.FC = () => {
                 <div className="socialWrap relative w-full flex justify-end mt-12 md:justify-center md:hidden">
                   <div className="icon relative max-w-[33px] h-[33px] ">
                     <Link
-                      href={"https://www.linkedin.com/company/bright-codeio/"}
+                      href={"https://twitter.com/brightcodeio"}
                       className="redirect"
                     >
                       .
@@ -949,7 +949,7 @@ const Home: React.FC = () => {
                   <div className="icon relative ml-3  max-w-[33px] h-[33px]">
                     <Link
                       href={
-                        "https://www.linkedin.com/in/bright-code-71120724a/"
+                        "https://www.linkedin.com/company/bright-codeio/"
                       }
                       className="redirect"
                     >
