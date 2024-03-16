cce1f27d3739671ed750e54427ee4b4ef67198af
Stefan Iordan <stefan>
2024-03-16 22:15:49 +0200

    created full service page

---
+++ pages/full-service.tsx
@@ -0,0 +1,375 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { useForm, ValidationError } from "@formspree/react";
+import Sticky from "../components/stickyNav";
+import ContactForm from "../components/contactForm";
+import { useState } from "react";
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
+// import type { Metadata } from "next";
+import Head from "next/head";
+import { useEffect } from "react";
+import { NextSeo } from "next-seo";
+
+export const metadata: Metadata = {
+  title: "White Label Drupal Development | Bright Code",
+  description:
+    " Discover Bright Code's White Label Web Development services, providing seamless solutions for agencies seeking reliable and confidential development support. Elevate your brand with our expert-driven, collaborative approach to web development",
+  openGraph: {
+    images: [
+      {
+        url: "/agency_agency-1.png",
+        alt: "Alt text for your image",
+      },
+    ],
+    siteName: "Bright Code",
+  },
+};
+
+interface OGImage {
+  url: string;
+  alt: string;
+}
+
+interface OpenGraph {
+  images: OGImage[];
+  siteName: string;
+}
+
+interface Metadata {
+  title: string;
+  description: string;
+  openGraph?: OpenGraph;
+}
+
+const stickyData = [
+  {
+    id: "1",
+    title: "Headless CMS",
+    url: "Headless CMS",
+  },
+  {
+    id: "2",
+    title: "Dedicated Development Team",
+    url: "dedicated-team",
+  },
+  {
+    id: "3",
+    title: "CMS Implementation",
+    url: "cms-implementation",
+  },
+  {
+    id: "4",
+    title: "CMS Maintenance",
+    url: "cms-support",
+  },
+];
+
+const WhiteLabelDevelopment: React.FC = () => {
+  const [clickedId, setClickedId] = useState<string | null>(null);
+  const [state, handleSubmit] = useForm("maygryee");
+  const [captcha, setcaptcha] = useState<string | null>();
+  const [formsuccess, setformsuccess] = useState(false);
+
+  const ClearForm = () => {
+    const inputs = document.querySelectorAll(".contactForm form input");
+    for (let i = 0; i < inputs.length; i++) {
+      const element = inputs[i] as HTMLInputElement;
+      element.value = "";
+    }
+  };
+
+  if (state.succeeded) {
+    if (!formsuccess) {
+      setformsuccess(true);
+      ClearForm();
+    }
+  }
+
+  const HideThankyouBox = () => {
+    setformsuccess(false);
+  };
+
+  return (
+    <>
+      <NextSeo
+        title={String(metadata.title)}
+        description={String(metadata.description)}
+        openGraph={
+          metadata.openGraph
+            ? {
+                title: String(metadata.title),
+                description: metadata.description || "", // Make sure it's not undefined
+                images: metadata.openGraph.images || [], // Make sure it's not undefined
+                siteName: metadata.openGraph.siteName || "", // Make sure it's not undefined
+              }
+            : undefined
+        }
+      />
+      {/* Banner Start */}
+      <section
+        className={`banner banner-second banner_DarkOverlay banner_bg_img bg-darkBlue level-two text-white `}
+        data-aos="fade-in"
+        data-aos-delay="500"
+        data-aos-duration="1000"
+      >
+        <div className="container">
+          <div className="w-full text-center relative z-10">
+            <h1>What we do?</h1>
+          </div>
+        </div>
+      </section>
+      {/* Banner End */}
+      {/* <Sticky ribbonVisible={true} data={stickyData} /> */}
+      <Sticky
+        ribbonVisible={true}
+        data={stickyData}
+        clickedId={clickedId}
+        setClickedId={setClickedId}
+      />
+
+      {/* White Label Development Intro Start */}
+      <section
+        className={`intro py-20 md:py-14 overflow-x-hidden `}
+        id="white-label-development"
+        data-aos="fade-up"
+        data-aos-delay="700"
+        data-aos-duration="1000"
+      >
+        <div className="container">
+          <div className="w-full z-1 relative">
+            <div
+              className="content w-full text-center relative"
+              data-aos="fade-right"
+              data-aos-delay="500"
+              data-aos-duration="1000"
+            >
+              <h6 className="title text-[#8000FF] uppercase font-normal mb-3 md:mb-1">
+                Unlocking Excellence.
+              </h6>
+              <h2 className="font-medium text-black mb-[38px] md:mb-[20px] ">
+                White Label Development.
+              </h2>
+            </div>
+            <div
+              className="content w-full text-center relative"
+              data-aos="fade-left"
+              data-aos-delay="500"
+              data-aos-duration="1000"
+            >
+              <h5 className="text-black font-light  w-full max-w-[960px] mx-auto">
+                In the world of web development, the invisible hand of expertise
+                often guides the most remarkable creations. At Bright Code, we
+                take pride in our role as the silent architects of digital
+                success, delivering excellence through White Label Development.
+              </h5>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* White Label Development Intro End */}
+
+      {/* Riverflow 1 Start */}
+      <section className="contentWithImage overflow-hidden">
+        <div className="container">
+          <div className="w-mainRow -ml-2.5 py-16 flex items-center z-1 relative md:flex-wrap md:w-full md:ml-0 featured md:py-12">
+            <div
+              className="content w-halfWidth mx-2.5 text-spaceBlack md:pr-0 md:w-full md:mx-0 md:mb-10"
+              data-aos="fade-right"
+              data-aos-delay="500"
+              data-aos-duration="1000"
+            >
+              <h6 className="text-[#8000FF] uppercase font-normal mb-4 md:mb-1">
+                ELEVATING WEB DEVELOPMENT
+              </h6>
+              <h2 className="font-medium xl-up:text-[45px] text-black mb-6">
+                Empowering Your Brand, Your Way
+              </h2>
+              <p className="mt-5 text-black">
+                Our White Label Development services are designed to empower
+                your brand. We act as your dedicated development team, working
+                hand-in-hand to meticulously craft web solutions that align
+                perfectly with your design vision.
+              </p>
+              <p className="mt-5 text-black">
+                The end result is a website that exemplifies your commitment to
+                excellence.
+              </p>
+            </div>
+            <div className="content imageWrap-outer w-halfWidth mx-2.5 md:w-full md:mx-0">
+              <div
+                className="imageWrap w-full h-[458px] tablet:h-[280px] sm:h-[260px] relative overflow-hidden rounded-[30px]  before:content-[''] before:absolute before:left-0 before:top-0 before:w-full before:h-full before:bg-transparent before:border-bordergradient2 before:border-[7px] before:z-10 before:border-solid before:rounded-[30px]"
+                data-aos="fade-left"
+                data-aos-delay="400"
+                data-aos-duration="500"
+              >
+                <Image
+                  src="/what-we-do/col-two-img1.png"
+                  alt="girl"
+                  width={600}
+                  height={500}
+                  className=" w-full h-full object-cover"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Riverflow 1 End */}
+
+      {/* Riverflow 2 Start */}
+      <section className="contentWithImage overflow-hidden">
+        <div className="container">
+          <div className="w-mainRow -ml-2.5 py-16 flex flex-row-reverse items-center z-1 relative md:flex-wrap md:w-full md:ml-0 md:py-12">
+            <div
+              className="content w-halfWidth mx-2.5 text-spaceBlack md:pl-0 md:w-full md:mx-0 md:mb-10"
+              data-aos="fade-left"
+              data-aos-delay="500"
+              data-aos-duration="1000"
+            >
+              <h6 className="text-[#8000FF] uppercase font-normal mb-4 md:mb-1">
+                Surpassing Milestones
+              </h6>
+              <h2 className="font-medium xl-up:text-[45px] text-black mb-6">
+                Exceeding Expectations, Every Time
+              </h2>
+              <p className="mt-5 text-black">
+                We go beyond the typical development approach. At Bright Code,
+                we&#39;re committed to elevating your reputation and ensuring
+                exceptional client satisfaction. By partnering with us, you
+                guarantee that your clients receive not just good websites, but
+                exceptional ones.
+              </p>
+              <p className="mt-5 text-black">
+                Our expertise in web development, combined with your creative
+                design prowess, creates a synergy that consistently surpasses
+                expectations.
+              </p>
+            </div>
+            <div className="content imageWrap-outer w-halfWidth mx-2.5 md:w-full md:mx-0">
+              <div
+                className="imageWrap w-full h-[458px] tablet:h-[280px] sm:h-[260px] relative overflow-hidden rounded-[30px]  before:content-[''] before:absolute before:left-0 before:top-0 before:w-full before:h-full before:bg-transparent before:border-bordergradient2 before:border-[7px] before:z-10 before:border-solid before:rounded-[30px]"
+                data-aos="fade-right"
+                data-aos-delay="400"
+                data-aos-duration="500"
+              >
+                <Image
+                  src="/what-we-do/col-two-img2.png"
+                  alt="girl"
+                  width={600}
+                  height={500}
+                  className=" w-full h-full object-cover"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Riverflow 2 End */}
+
+      {/* Riverflow 3 Start */}
+      <section className="contentWithImage overflow-hidden">
+        <div className="container">
+          <div className="w-mainRow -ml-2.5 py-16 flex items-center z-1 relative md:flex-wrap md:w-full md:ml-0 featured md:py-12">
+            <div
+              className="content w-halfWidth mx-2.5 text-spaceBlack md:pr-0 md:w-full md:mx-0 md:mb-10"
+              data-aos="fade-right"
+              data-aos-delay="500"
+              data-aos-duration="1000"
+            >
+              <h6 className="text-[#8000FF] uppercase font-normal mb-4 md:mb-1">
+                Harmonizing Success
+              </h6>
+              <h2 className="font-medium xl-up:text-[45px] text-black mb-6">
+                The Power of Collaboration
+              </h2>
+              <p className="mt-5 text-black">
+                Collaboration is at the core of what we do. By choosing us for
+                your web development needs, you open doors to a world of
+                possibilities.
+              </p>
+              <p className="mt-5 text-black">
+                Our collaborative culture and commitment to professionalism
+                ensure that your clients receive the best of both worlds: your
+                design excellence and our technical expertise.
+              </p>
+            </div>
+            <div className="content imageWrap-outer w-halfWidth mx-2.5 md:w-full md:mx-0">
+              <div
+                className="imageWrap w-full h-[458px] tablet:h-[280px] sm:h-[260px] relative overflow-hidden rounded-[30px]  before:content-[''] before:absolute before:left-0 before:top-0 before:w-full before:h-full before:bg-transparent before:border-bordergradient2 before:border-[7px] before:z-10 before:border-solid before:rounded-[30px]"
+                data-aos="fade-left"
+                data-aos-delay="400"
+                data-aos-duration="500"
+              >
+                <Image
+                  src="/what-we-do/col-two-img3.png"
+                  alt="girl"
+                  width={600}
+                  height={500}
+                  className=" w-full h-full object-cover"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Riverflow 3 End */}
+
+      {/* Riverflow 4 Start */}
+      <section className="contentWithImage overflow-hidden pb-16 md:pb-8">
+        <div className="container">
+          <div className="w-mainRow -ml-2.5 py-16 flex flex-row-reverse items-center z-1 relative md:flex-wrap md:w-full md:ml-0 md:py-12">
+            <div
+              className="content w-halfWidth mx-2.5 text-spaceBlack md:pl-0 md:w-full md:mx-0 md:mb-10"
+              data-aos="fade-left"
+              data-aos-delay="500"
+              data-aos-duration="1000"
+            >
+              <h6 className="text-[#8000FF] uppercase font-normal mb-4 md:mb-1">
+                SUPPORTING YOUR BRAND, OUR CORE COMMITMENT
+              </h6>
+              <h2 className="font-medium xl-up:text-[45px] text-black mb-6">
+                Your Success, Our Priority
+              </h2>
+              <p className="mt-5 text-black">
+                At Bright Code, we are more than just a development agency; we
+                are your trusted partner for success. White Label Development is
+                a canvas where your brand shines. Delight your clients, exceed
+                their expectations, and elevate your agency&#39;s reputation
+                with Bright Code as your collaborative partner in digital
+                excellence.
+              </p>
+              <p className="mt-5 text-black">
+                Experience the power of White Label Development with Bright
+                Code. Your success is our success, and together, we&#39;ll excel
+                in the world of web solutions.
+              </p>
+            </div>
+            <div className="content imageWrap-outer w-halfWidth mx-2.5 md:w-full md:mx-0">
+              <div
+                className="imageWrap w-full h-[458px] tablet:h-[280px] sm:h-[260px] relative overflow-hidden rounded-[30px]  before:content-[''] before:absolute before:left-0 before:top-0 before:w-full before:h-full before:bg-transparent before:border-bordergradient2 before:border-[7px] before:z-10 before:border-solid before:rounded-[30px]"
+                data-aos="fade-right"
+                data-aos-delay="400"
+                data-aos-duration="500"
+              >
+                <Image
+                  src="/what-we-do/col-two-img4.png"
+                  alt="girl"
+                  width={600}
+                  height={500}
+                  className=" w-full h-full object-cover"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Riverflow 4 End */}
+     
+        <ContactForm/>
+    </>
+  );
+};
+
+export default WhiteLabelDevelopment;
