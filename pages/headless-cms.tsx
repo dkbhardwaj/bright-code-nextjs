2988fea8c2f780a6db4b2924b4bcc1428dca6377
Stefan Iordan <stefan>
2024-03-17 12:07:34 +0200

    headless cms link in full service page

---
+++ pages/headless-cms.tsx
@@ -0,0 +1,392 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { useForm, ValidationError } from "@formspree/react";
+import Sticky from "../components/stickyNav";
+import ContactForm from "../components/contactForm";
+import { useState } from "react";
+// import type { Metadata } from "next";
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
+import Head from "next/head";
+import { useEffect } from "react";
+import { NextSeo } from "next-seo";
+
+export const metadata: Metadata = {
+  title:
+    "Efficient Drupal Implementation Services for Seamless Content Management | Bright Code",
+  description:
+    "Explore Bright Code's Drupal Implementation services, designed for seamless content management. Elevate your digital presence with our expert-driven solutions, ensuring efficient and user-friendly CMS integration tailored to your unique needs.",
+  openGraph: {
+    images: [
+      {
+        url: "/agency_agency-1.png",
+        alt: "Alt text for your image",
+      },
+    ],
+    siteName: "Bright Code",
+  },
+};
+
+interface OGImage {
+  url: string;
+  alt: string;
+}
+
+interface OpenGraph {
+  images: OGImage[];
+  siteName: string;
+}
+
+interface Metadata {
+  title: string;
+  description: string;
+  openGraph?: OpenGraph;
+}
+
+interface StickyItem {
+  id: string;
+  title: string;
+  url: string;
+}
+
+const CmsImplementation: React.FC = () => {
+  const [clickedId, setClickedId] = useState<string | null>(null);
+  const [state, handleSubmit] = useForm("maygryee");
+  const [captcha, setcaptcha] = useState<string | null>();
+  const [formsuccess, setformsuccess] = useState(false);
+
+  const [winWidth, setWinWidth] = useState(0);
+  useEffect(() => {
+    const handleResize = () => {
+      setWinWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const ClearForm = () => {
+    const inputs = document.querySelectorAll(".contactForm form input");
+    for (let i = 0; i < inputs.length; i++) {
+      const element = inputs[i] as HTMLInputElement;
+      element.value = "";
+    }
+  };
+
+  if (state.succeeded) {
+    if (!formsuccess) {
+      setformsuccess(true);
+      ClearForm();
+    }
+  }
+
+  const HideThankyouBox = () => {
+    setformsuccess(false);
+  };
+
+  const desktopStickyData: StickyItem[] = [
+    {
+      id: "1",
+      title: "White Label Development",
+      url: "white-label-development",
+    },
+    {
+      id: "2",
+      title: "Dedicated Development Team",
+      url: "dedicated-team",
+    },
+    {
+      id: "3",
+      title: "CMS Implementation",
+      url: "cms-implementation",
+    },
+    {
+      id: "4",
+      title: "CMS Maintenance",
+      url: "cms-support",
+    },
+  ];
+
+  const mobileStickyData: StickyItem[] = [
+    {
+      id: "1",
+      title: "CMS Implementation",
+      url: "cms-implementation",
+    },
+    {
+      id: "2",
+      title: "White Label Development",
+      url: "white-label-development",
+    },
+    {
+      id: "3",
+      title: "Dedicated Development Team",
+      url: "dedicated-team",
+    },
+    {
+      id: "4",
+      title: "CMS Maintenance",
+      url: "cms-support",
+    },
+  ];
+
+  const stickyData = winWidth > 991 ? desktopStickyData : mobileStickyData;
+
+  return (
+    <>
+      <NextSeo
+        title={String(metadata.title)}
+        description={String(metadata.description)}
+        openGraph={
+          metadata.openGraph
+            ? {
+                title: String(metadata.title),
+                description: metadata.description || "", // Make sure it's not undefined
+                images: metadata.openGraph.images || [], // Make sure it's not undefined
+                siteName: metadata.openGraph.siteName || "", // Make sure it's not undefined
+              }
+            : undefined
+        }
+      />
+      {/* Banner Start */}
+      <section
+        className={`banner banner-second banner_DarkOverlay banner_bg_img bg-darkBlue level-two text-white`}
+        data-aos="fade-in"
+        data-aos-delay="500"
+        data-aos-duration="1000"
+      >
+        <div className="container">
+          <div className="w-full text-center relative z-10">
+            <h1>What we do?</h1>
+          </div>
+        </div>
+      </section>
+      {/* Banner End */}
+      {/* <Sticky ribbonVisible={true} data={stickyData} /> */}
+      <Sticky
+        ribbonVisible={true}
+        data={stickyData}
+        clickedId={clickedId}
+        setClickedId={setClickedId}
+      />
+
+      {/* CMS Implementation Intro Start */}
+      <section
+        className={`intro py-20 md:py-14 overflow-x-hidden`}
+        id="white-label-development"
+        data-aos="fade-up"
+        data-aos-delay="700"
+        data-aos-duration="1000"
+      >
+        <div className="container">
+          <div className="w-full z-1 relative text-center">
+            <h6 className="text-[#8000FF] uppercase font-normal mb-3 md:mb-1">
+              Empowering Your Content
+            </h6>
+            <h2 className="font-medium text-black mb-[38px] md:mb-[20px] ">
+              CMS Implementation
+            </h2>
+            <h5 className="text-black font-light w-full max-w-[960px] mx-auto">
+              In the ever-evolving digital landscape, your content is the
+              cornerstone of your online presence. At Bright Code, we understand
+              the significance of efficient Content Management System (CMS)
+              Implementation in ensuring your brand's message is delivered
+              seamlessly to your audience.
+            </h5>
+          </div>
+        </div>
+      </section>
+      {/*  CMS Implementation Intro End */}
+
+      {/* Riverflow 1 Start */}
+      <section className="contentWithImage overflow-hidden">
+        <div className="container">
+          <div className="w-mainRow -ml-2.5  py-16 flex items-center z-1 relative md:flex-wrap md:w-full md:ml-0  featured md:py-12">
+            <div
+              className="content w-halfWidth mx-2.5 text-spaceBlack md:pr-0 md:w-full md:mx-0 md:mb-10"
+              data-aos="fade-right"
+              data-aos-delay="500"
+              data-aos-duration="1000"
+            >
+              <h6 className="text-[#8000FF] uppercase font-normal mb-4 md:mb-1">
+                CMS Implementation
+              </h6>
+              <h2 className="font-medium xl-up:text-[45px] text-black mb-6">
+                What is Drupal Implementation?
+              </h2>
+              <p className="mt-5 text-black">
+                Drupal Implementation is the art of transforming your vision
+                into a dynamic, user-friendly Drupal platform. At Bright Code,
+                we specialize in taking your content management needs and
+                translating them into a robust, tailor-made solution. We make
+                the process of managing your digital content effortless, so you
+                can focus on what truly matters – your message.
+              </p>
+            </div>
+            <div className="content imageWrap-outer w-halfWidth mx-2.5 md:w-full md:mx-0">
+              <div
+                className="imageWrap w-full h-[458px] tablet:h-[280px] sm:h-[260px] relative overflow-hidden rounded-[30px]  before:content-[''] before:absolute before:left-0 before:top-0 before:w-full before:h-full before:bg-transparent before:border-bordergradient2 before:border-[7px] before:z-10 before:border-solid before:rounded-[30px]"
+                data-aos="fade-left"
+                data-aos-delay="500"
+                data-aos-duration="1000"
+              >
+                <Image
+                  src="/what-we-do/col-two3-img1.png"
+                  alt="girl"
+                  width={600}
+                  height={500}
+                  className=" w-full h-full object-cover"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Riverflow 1 End */}
+
+      {/* Riverflow 2 Start */}
+      <section className="contentWithImage overflow-hidden">
+        <div className="container">
+          <div className="w-mainRow -ml-2.5 py-16 flex flex-row-reverse items-center z-1 relative md:flex-wrap md:w-full md:ml-0 md:py-12">
+            <div
+              className="content w-halfWidth mx-2.5 text-spaceBlack md:pl-0 md:w-full md:mx-0 md:mb-10"
+              data-aos="fade-left"
+              data-aos-delay="500"
+              data-aos-duration="1000"
+            >
+              <h6 className="text-[#8000FF] uppercase font-normal mb-4 md:mb-1">
+                Strengthening Your Brand
+              </h6>
+              <h2 className="font-medium xl-up:text-[45px] text-black mb-6">
+                Empowering Your Brand, Your Way
+              </h2>
+              <p className="mt-5 text-black">
+                Our CMS Implementation services are designed to empower your
+                brand's digital voice. We don't just offer off-the-shelf
+                solutions; we craft customized CMS platforms that perfectly
+                align with your brand's unique identity and objectives. Whether
+                you're a design agency or a CEO looking to enhance your online
+                presence, we collaborate closely with you to ensure the system
+                we implement is a seamless fit.
+              </p>
+            </div>
+            <div className="content imageWrap-outer w-halfWidth mx-2.5 md:w-full md:mx-0">
+              <div
+                className="imageWrap w-full h-[458px] tablet:h-[280px] sm:h-[260px] relative overflow-hidden rounded-[30px]  before:content-[''] before:absolute before:left-0 before:top-0 before:w-full before:h-full before:bg-transparent before:border-bordergradient2 before:border-[7px] before:z-10 before:border-solid before:rounded-[30px]"
+                data-aos="fade-right"
+                data-aos-delay="500"
+                data-aos-duration="1000"
+              >
+                <Image
+                  src="/what-we-do/col-two3-img2.png"
+                  alt="girl"
+                  width={600}
+                  height={500}
+                  className=" w-full h-full object-cover"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Riverflow 2 End */}
+
+      {/* Riverflow 3 Start */}
+      <section className="contentWithImage overflow-hidden">
+        <div className="container">
+          <div className="w-mainRow -ml-2.5 py-16 flex items-center z-1 relative md:flex-wrap md:w-full md:ml-0  featured md:py-12">
+            <div
+              className="content w-halfWidth mx-2.5 text-spaceBlack md:pr-0 md:w-full md:mx-0 md:mb-10"
+              data-aos="fade-right"
+              data-aos-delay="500"
+              data-aos-duration="1000"
+            >
+              <h6 className="text-[#8000FF] uppercase font-normal mb-4 md:mb-1">
+                Beyond Drupal Delivery
+              </h6>
+              <h2 className="font-medium xl-up:text-[45px] text-black mb-6">
+                Exceeding Expectations, Every Time
+              </h2>
+              <p className="mt-5 text-black">
+                We don't stop at just delivering a CMS; we aim to exceed your
+                expectations. With Bright Code, you can trust that your CMS will
+                be not only user-friendly but also capable of managing your
+                content efficiently. Our technical expertise combines with your
+                content strategies, creating a synergy that enables you to
+                achieve unparalleled results.
+              </p>
+            </div>
+            <div className="content imageWrap-outer w-halfWidth mx-2.5 md:w-full md:mx-0">
+              <div
+                className="imageWrap w-full h-[458px] tablet:h-[280px] sm:h-[260px] relative overflow-hidden rounded-[30px]  before:content-[''] before:absolute before:left-0 before:top-0 before:w-full before:h-full before:bg-transparent before:border-bordergradient2 before:border-[7px] before:z-10 before:border-solid before:rounded-[30px]"
+                data-aos="fade-left"
+                data-aos-delay="500"
+                data-aos-duration="1000"
+              >
+                <Image
+                  src="/what-we-do/col-two3-img3.png"
+                  alt="girl"
+                  width={600}
+                  height={500}
+                  className=" w-full h-full object-cover"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Riverflow 3 End */}
+
+      {/* Riverflow 4 Start */}
+      <section className="contentWithImage overflow-hidden pb-16 md:pb-8">
+        <div className="container">
+          <div className="w-mainRow -ml-2.5 py-16 flex flex-row-reverse items-center z-1 relative md:flex-wrap md:w-full md:ml-0 md:py-12">
+            <div
+              className="content w-halfWidth mx-2.5 text-spaceBlack md:pl-0 md:w-full md:mx-0 md:mb-10"
+              data-aos="fade-left"
+              data-aos-delay="500"
+              data-aos-duration="1000"
+            >
+              <h6 className="text-[#8000FF] uppercase font-normal mb-4 md:mb-1">
+                Unlocking Success
+              </h6>
+              <h2 className="font-medium xl-up:text-[45px] text-black mb-6">
+                The Power of Collaboration
+              </h2>
+              <p className="mt-5 text-black">
+                Collaboration is at the heart of what we do. By partnering with
+                Bright Code, you gain access to a team of experts who are fully
+                aligned with your content management objectives. Our
+                collaborative culture and commitment to professionalism ensure
+                that your CMS is implemented seamlessly and efficiently.
+              </p>
+            </div>
+            <div className="content imageWrap-outer w-halfWidth mx-2.5 md:w-full md:mx-0">
+              <div
+                className="imageWrap w-full h-[458px] tablet:h-[280px] sm:h-[260px] relative overflow-hidden rounded-[30px]  before:content-[''] before:absolute before:left-0 before:top-0 before:w-full before:h-full before:bg-transparent before:border-bordergradient2 before:border-[7px] before:z-10 before:border-solid before:rounded-[30px]"
+                data-aos="fade-right"
+                data-aos-delay="500"
+                data-aos-duration="1000"
+              >
+                <Image
+                  src="/what-we-do/col-two3-img4.png"
+                  alt="girl"
+                  width={600}
+                  height={500}
+                  className=" w-full h-full object-cover"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Riverflow 4 End */}
+      <ContactForm/>
+    </>
+  );
+};
+
+export default CmsImplementation;
