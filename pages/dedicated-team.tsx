5ec386c89b002a5152f180942d8bf9d1a55b910a
Bhawana <bhawana@webenertia.com>
2024-04-03 20:14:55 +0530

    create dedicated-team page components

--- pages/dedicated-team.tsx
+++ pages/dedicated-team.tsx
@@ -11,6 +11,16 @@ import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 import Head from "next/head";
 import { useEffect } from "react";
 import { NextSeo } from "next-seo";
+import Overview from "../components/Overview";
+import BannerThird from "../components/BannerThird";
+import ContentWithImageColTwo from "../components/ContentWithImageColTwo";
+import {
+  overview,
+  contentWithImage,
+  contentWithImage2,
+  contentWithImage3,
+  contentWithImage4,
+} from "../dataDedicatedTeam/data";
 
 export const metadata: Metadata = {
   title: "Empower Your Projects with Dedicated Development Teams | Bright Code",
@@ -150,20 +160,9 @@ const DedicatedTeam: React.FC = () => {
             : undefined
         }
       />
-      {/* Banner Start */}
-      <section
-        className={`banner banner-second banner_DarkOverlay banner_bg_img bg-darkBlue level-two text-white`}
-        data-aos="fade-in"
-        data-aos-delay="500"
-        data-aos-duration="1000"
-      >
-        <div className="container">
-          <div className="w-full text-center relative z-10">
-            <h1>What we do?</h1>
-          </div>
-        </div>
-      </section>
-      {/* Banner End */}
+
+      <BannerThird />
+
       {/* <Sticky ribbonVisible={true} data={stickyData} /> */}
       <Sticky
         ribbonVisible={true}
@@ -172,222 +171,17 @@ const DedicatedTeam: React.FC = () => {
         setClickedId={setClickedId}
       />
 
-      {/* Dedicated Development Team Intro Start */}
-      <section
-        className={`intro py-20 md:py-14 overflow-x-hidden`}
-        id="white-label-development"
-        data-aos="fade-up"
-        data-aos-delay="700"
-        data-aos-duration="1000"
-      >
-        <div className="container">
-          <div className="w-full z-1 relative text-center">
-            <h6 className="text-[#8000FF] uppercase font-normal mb-3 md:mb-1">
-              Elevate Your Digital Vision.
-            </h6>
-            <h2 className="font-medium text-black mb-[38px] md:mb-[20px] ">
-              Dedicated Dev Team
-            </h2>
-            <h5 className="text-black font-light w-full max-w-[960px] mx-auto">
-              In the dynamic landscape of web development, the true mark of
-              excellence is achieved through collaboration and a dedicated focus
-              on your unique goals. At Bright Code, we're proud to be your
-              partners in this journey, offering Dedicated Development Teams
-              that bring your digital dreams to life.
-            </h5>
-          </div>
-        </div>
-      </section>
-      {/* Dedicated Development Team Intro End */}
+      <Overview data={overview} />
 
-      {/* Riverflow 1 Start */}
-      <section className="contentWithImage overflow-hidden">
-        <div className="container">
-          <div className="w-mainRow -ml-2.5  py-16 flex items-center z-1 relative md:flex-wrap md:w-full md:ml-0  featured md:py-12">
-            <div
-              className="content w-halfWidth mx-2.5 text-spaceBlack md:pr-0 md:w-full md:mx-0 md:mb-10"
-              data-aos="fade-right"
-              data-aos-delay="500"
-              data-aos-duration="1000"
-            >
-              <h6 className="text-[#8000FF] uppercase font-normal mb-4 md:mb-1">
-                TAILORED EXCELLENCE: DEDICATED DEV TEAMS
-              </h6>
-              <h2 className="font-medium xl-up:text-[45px] text-black mb-6">
-                What are Dedicated Development Teams?
-              </h2>
-              <p className="mt-5 text-black">
-                Our Dedicated Development Teams are the embodiment of unwavering
-                commitment and expertise. We tailor our teams to match your
-                specific needs, seamlessly integrating with your agency to
-                become your in-house development arm. This collaborative
-                approach ensures a harmonious blend of our technical proficiency
-                and your creative vision.
-              </p>
-            </div>
-            <div className="content imageWrap-outer w-halfWidth mx-2.5 md:w-full md:mx-0">
-              <div
-                className="imageWrap w-full h-[458px] tablet:h-[280px] sm:h-[260px] relative overflow-hidden rounded-[30px]  before:content-[''] before:absolute before:left-0 before:top-0 before:w-full before:h-full before:bg-transparent before:border-bordergradient2 before:border-[7px] before:z-10 before:border-solid before:rounded-[30px]"
-                data-aos="fade-left"
-                data-aos-delay="500"
-                data-aos-duration="1000"
-              >
-                <Image
-                  src="/what-we-do/col-two2-img1.png"
-                  alt="girl"
-                  width={600}
-                  height={500}
-                  className=" w-full h-full object-cover"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      {/* Riverflow 1 End */}
+      <ContentWithImageColTwo data={contentWithImage} />
 
-      {/* Riverflow 2 Start */}
-      <section className="contentWithImage overflow-hidden">
-        <div className="container">
-          <div className="w-mainRow -ml-2.5 py-16 flex flex-row-reverse items-center z-1 relative md:flex-wrap md:w-full md:ml-0 md:py-12">
-            <div
-              className="content w-halfWidth mx-2.5 text-spaceBlack md:pl-0 md:w-full md:mx-0 md:mb-10"
-              data-aos="fade-left"
-              data-aos-delay="500"
-              data-aos-duration="1000"
-            >
-              <h6 className="text-[#8000FF] uppercase font-normal mb-4 md:mb-1">
-                ELEVATING YOUR DIGITAL VISION
-              </h6>
-              <h2 className="font-medium xl-up:text-[45px] text-black mb-6">
-                Empowering Your Vision, Your Way
-              </h2>
-              <p className="mt-5 text-black">
-                Our Dedicated Development Teams are designed to empower your
-                digital vision without compromise. We operate as an extension of
-                your in-house team, diligently working together to transform
-                your concepts into polished, high-performing web solutions. The
-                result is a digital masterpiece that reflects your brand's
-                unique identity and your dedication to excellence.
-              </p>
-            </div>
-            <div className="content imageWrap-outer w-halfWidth mx-2.5 md:w-full md:mx-0">
-              <div
-                className="imageWrap w-full h-[458px] tablet:h-[280px] sm:h-[260px] relative overflow-hidden rounded-[30px]  before:content-[''] before:absolute before:left-0 before:top-0 before:w-full before:h-full before:bg-transparent before:border-bordergradient2 before:border-[7px] before:z-10 before:border-solid before:rounded-[30px]"
-                data-aos="fade-right"
-                data-aos-delay="500"
-                data-aos-duration="1000"
-              >
-                <Image
-                  src="/what-we-do/col-two2-img2.png"
-                  alt="girl"
-                  width={600}
-                  height={500}
-                  className=" w-full h-full object-cover"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      {/* Riverflow 2 End */}
+      <ContentWithImageColTwo data={contentWithImage2} />
 
-      {/* Riverflow 3 Start */}
-      <section className="contentWithImage overflow-hidden">
-        <div className="container">
-          <div className="w-mainRow -ml-2.5 py-16 flex items-center z-1 relative md:flex-wrap md:w-full md:ml-0  featured md:py-12">
-            <div
-              className="content w-halfWidth mx-2.5 text-spaceBlack md:pr-0 md:w-full md:mx-0 md:mb-10"
-              data-aos="fade-right"
-              data-aos-delay="500"
-              data-aos-duration="1000"
-            >
-              <h6 className="text-[#8000FF] uppercase font-normal mb-4 md:mb-1">
-                UNLEASHING POTENTIAL
-              </h6>
-              <h2 className="font-medium xl-up:text-[45px] text-black mb-6">
-                Exceeding Expectations, Every Time
-              </h2>
-              <p className="mt-5 text-black">
-                We don't simply deliver development solutions; we consistently
-                surpass expectations. Our expertise, combined with your
-                creativity, fosters a synergy that elevates the quality of your
-                projects. With Bright Code's Dedicated Development Teams, you
-                can be confident that your clients will receive exceptional
-                results.
-              </p>
-              <p className="mt-5 text-black">
-                Our collaborative culture and commitment to professionalism
-                ensure that your clients receive the best of both worlds: your
-                design excellence and our technical expertise.
-              </p>
-            </div>
-            <div className="content imageWrap-outer w-halfWidth mx-2.5 md:w-full md:mx-0">
-              <div
-                className="imageWrap w-full h-[458px] tablet:h-[280px] sm:h-[260px] relative overflow-hidden rounded-[30px]  before:content-[''] before:absolute before:left-0 before:top-0 before:w-full before:h-full before:bg-transparent before:border-bordergradient2 before:border-[7px] before:z-10 before:border-solid before:rounded-[30px]"
-                data-aos="fade-left"
-                data-aos-delay="500"
-                data-aos-duration="1000"
-              >
-                <Image
-                  src="/what-we-do/col-two2-img3.png"
-                  alt="girl"
-                  width={600}
-                  height={500}
-                  className=" w-full h-full object-cover"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      {/* Riverflow 3 End */}
+      <ContentWithImageColTwo data={contentWithImage3} />
 
-      {/* Riverflow 4 Start */}
-      <section className="contentWithImage overflow-hidden pb-16 md:pb-8">
-        <div className="container">
-          <div className="w-mainRow -ml-2.5 py-16 flex flex-row-reverse items-center z-1 relative md:flex-wrap md:w-full md:ml-0 md:py-12">
-            <div
-              className="content w-halfWidth mx-2.5 text-spaceBlack md:pl-0 md:w-full md:mx-0 md:mb-10"
-              data-aos="fade-left"
-              data-aos-delay="500"
-              data-aos-duration="1000"
-            >
-              <h6 className="text-[#8000FF] uppercase font-normal mb-4 md:mb-1">
-                EMPOWERING SUCCESS THROUGH PARTNERSHIP
-              </h6>
-              <h2 className="font-medium xl-up:text-[45px] text-black mb-6">
-                The Power of Collaboration
-              </h2>
-              <p className="mt-5 text-black">
-                Collaboration is the foundation of our approach. By partnering
-                with Bright Code, you gain access to a pool of experts who are
-                fully aligned with your objectives. Our collaborative culture
-                and commitment to professionalism ensure that your projects are
-                executed with precision and excellence.
-              </p>
-            </div>
-            <div className="content imageWrap-outer w-halfWidth mx-2.5 md:w-full md:mx-0">
-              <div
-                className="imageWrap w-full h-[458px] tablet:h-[280px] sm:h-[260px] relative overflow-hidden rounded-[30px]  before:content-[''] before:absolute before:left-0 before:top-0 before:w-full before:h-full before:bg-transparent before:border-bordergradient2 before:border-[7px] before:z-10 before:border-solid before:rounded-[30px]"
-                data-aos="fade-right"
-                data-aos-delay="500"
-                data-aos-duration="1000"
-              >
-                <Image
-                  src="/what-we-do/col-two2-img4.png"
-                  alt="girl"
-                  width={600}
-                  height={500}
-                  className=" w-full h-full object-cover"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      {/* Riverflow 4 End */}
-      <ContactForm/>
+      <ContentWithImageColTwo data={contentWithImage4} />
+
+      <ContactForm />
     </>
   );
 };
