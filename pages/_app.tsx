c2c52829737878f644d7585b5011d3490e34b4b3
Bhawana <bhawana@webenertia.com>
2024-04-08 18:01:42 +0530

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -48,42 +48,42 @@ export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   useEffect(() => {
     initAOS(); // Initialize AOS when the app mounts on the client side
   }, []);
-  const gtagScript = `<script type=‘text/javascript’ src=‘https://www.googletagmanager.com/gtag/js?id=UA-254354410-1’ id=‘google_gtagjs-js’ async></script>
-  <script type=‘text/javascript’ id=‘google_gtagjs-js-after’>
-  window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}
-  gtag(‘set’, ‘linker’, {“domains”:[“www.bright-code.io”]} );
-  gtag(“js”, new Date());
-  gtag(“set”, “developer_id.dZTNiMT”, true);
-  gtag(“config”, “UA-254354410-1”, {“anonymize_ip”:true});
-  gtag(“config”, “G-0J63B1FNF9");
-  </script>`;
-  const gtagManagerScript = `<script type=“text/javascript”>
-  ( function( w, d, s, l, i ) {
-    w[l] = w[l] || [];
-    w[l].push( {‘gtm.start’: new Date().getTime(), event: ‘gtm.js’} );
-    var f = d.getElementsByTagName( s )[0],
-      j = d.createElement( s ), dl = l != ‘dataLayer’ ? ‘&l=’ + l : ‘’;
-    j.async = true;
-    j.src = ‘https://www.googletagmanager.com/gtm.js?id=’ + i + dl;
-    f.parentNode.insertBefore( j, f );
-  } )( window, document, ‘script’, ‘dataLayer’, ‘GTM-KVK3JT9’ );
-  
-</script>`;
-  const googleTagScript = `<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-11070673099"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'AW-11070673099');
-</script>`;
+  //   const gtagScript = `<script type=‘text/javascript’ src=‘https://www.googletagmanager.com/gtag/js?id=UA-254354410-1’ id=‘google_gtagjs-js’ async></script>
+  //   <script type=‘text/javascript’ id=‘google_gtagjs-js-after’>
+  //   window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}
+  //   gtag(‘set’, ‘linker’, {“domains”:[“www.bright-code.io”]} );
+  //   gtag(“js”, new Date());
+  //   gtag(“set”, “developer_id.dZTNiMT”, true);
+  //   gtag(“config”, “UA-254354410-1”, {“anonymize_ip”:true});
+  //   gtag(“config”, “G-0J63B1FNF9");
+  //   </script>`;
+  //   const gtagManagerScript = `<script type=“text/javascript”>
+  //   ( function( w, d, s, l, i ) {
+  //     w[l] = w[l] || [];
+  //     w[l].push( {‘gtm.start’: new Date().getTime(), event: ‘gtm.js’} );
+  //     var f = d.getElementsByTagName( s )[0],
+  //       j = d.createElement( s ), dl = l != ‘dataLayer’ ? ‘&l=’ + l : ‘’;
+  //     j.async = true;
+  //     j.src = ‘https://www.googletagmanager.com/gtm.js?id=’ + i + dl;
+  //     f.parentNode.insertBefore( j, f );
+  //   } )( window, document, ‘script’, ‘dataLayer’, ‘GTM-KVK3JT9’ );
 
-  useEffect(() => {
-    let head = document.getElementsByTagName("head")[0];
-    head.innerHTML += gtagScript;
-    head.innerHTML += gtagManagerScript;
-    head.innerHTML += googleTagScript;
-  }, []);
+  // </script>`;
+  //   const googleTagScript = `<!-- Google tag (gtag.js) -->
+  // <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11070673099"></script>
+  // <script>
+  //   window.dataLayer = window.dataLayer || [];
+  //   function gtag(){dataLayer.push(arguments);}
+  //   gtag('js', new Date());
+  //   gtag('config', 'AW-11070673099');
+  // </script>`;
+
+  // useEffect(() => {
+  //   let head = document.getElementsByTagName("head")[0];
+  //   head.innerHTML += gtagScript;
+  //   head.innerHTML += gtagManagerScript;
+  //   head.innerHTML += googleTagScript;
+  // }, []);
 
   const router = useRouter();
   // const [currentUrl, setCanonicalUrl] = useState("");
