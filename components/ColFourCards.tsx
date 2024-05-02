43c984863ba08b442b8b4a925ef4d74e9561f356
Bhawana <bhawana@webenertia.com>
2024-05-02 20:21:01 +0530

    update colFourCards

--- components/ColFourCards.tsx
+++ components/ColFourCards.tsx
@@ -25,21 +25,25 @@ const ColFourCards: React.FC<colFourCardData> = ({ data }) => {
               data-aos-duration="500"
               key={item.id}
             >
-              <div className="icon-wrap mx-auto max-w-[41px] h-[41px] before:content-[''] relative mt-[15px] mb-9 before:absolute before:left-[-15px] before:top-[-15px] before:w-iconBeforeW before:h-iconBeforeH before:bg-[#ffffff2b] before:rounded-[15px] before:transition-colors before:duration-500 hover:before:bg-purplePink">
-                <Image
-                  src={item.iconUrl}
-                  alt="icon"
-                  width={45}
-                  height={45}
-                  loading="lazy"
-                  className="w-full h-full object-contain relative z-10"
+              {item.iconUrl && (
+                <div className="icon-wrap mx-auto max-w-[41px] h-[41px] before:content-[''] relative mt-[15px] mb-9 before:absolute before:left-[-15px] before:top-[-15px] before:w-iconBeforeW before:h-iconBeforeH before:bg-[#ffffff2b] before:rounded-[15px] before:transition-colors before:duration-500 hover:before:bg-purplePink">
+                  <Image
+                    src={item.iconUrl}
+                    alt="icon"
+                    width={45}
+                    height={45}
+                    loading="lazy"
+                    className="w-full h-full object-contain relative z-10"
+                  />
+                </div>
+              )}
+              {item.title && <h5 className="text-white">{item.title}</h5>}
+              {item.paragraph && (
+                <p
+                  className="mt-4 text-[15px] text-lightGray leading-[26px] "
+                  dangerouslySetInnerHTML={{ __html: item.paragraph }}
                 />
-              </div>
-              <h5 className="text-white">{item.title}</h5>
-              <p
-                className="mt-4 text-[15px] text-lightGray leading-[26px] "
-                dangerouslySetInnerHTML={{ __html: item.paragraph }}
-              />
+              )}
             </div>
           ))}
         </div>
