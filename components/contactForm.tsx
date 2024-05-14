cab5ebd5166fc96463c17f846e31559b2885c28d
Ganesh <ganesh@webenertia.com>
2024-05-14 19:47:59 +0530

    enhancing code

--- components/contactForm.tsx
+++ components/contactForm.tsx
@@ -1,8 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useForm, ValidationError } from "@formspree/react";
-import { useState } from "react";
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 
 interface ContactFormProps {
@@ -142,7 +141,6 @@ const ContactForm: React.FC<ContactFormProps> = ({ data }) => {
                         type="subject"
                         id="subject"
                         name="subject"
-                        // value={values.subject}
                         required={true}
                         className="w-full relative mt-2 py-[14px] bg-white rounded-[9px] border border-white border-solid border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 px-3 leading-8 transition-colors duration-200 ease-in-out"
                       />
@@ -162,7 +160,6 @@ const ContactForm: React.FC<ContactFormProps> = ({ data }) => {
                       <textarea
                         id="message"
                         name="message"
-                        // value={values.message}
                         className="w-full relative mt-2 py-[14px] bg-white rounded-[9px] border border-white border-solid border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-[106px] text-base outline-none text-gray-700 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
                       ></textarea>
                     </div>
@@ -222,7 +219,7 @@ const ContactForm: React.FC<ContactFormProps> = ({ data }) => {
         <section className="thank_you_overlay fixed top-0 left-0 w-[100vw] h-[100vh] bg-[#000000b5] flex justify-center items-center z-[60] ">
           <div className="container">
             <div className="thankU_overlay relative bg-white rounded-md min-h-[600px] p-10 flex justify-center items-center z-20 ">
-              <div
+              <button
                 className="close_icon max-w-[34px] h-[34px] absolute top-5 right-5 cursor-pointer "
                 onClick={HideThankyouBox}
               >
@@ -234,7 +231,7 @@ const ContactForm: React.FC<ContactFormProps> = ({ data }) => {
                   className=" w-full h-full object-contain "
                   alt="close"
                 />
-              </div>
+              </button>
               <div className="thankYouBox !min-h-fit text-center">
                 <div className="thankU_check_icon mx-auto max-w-[112px] h-[112px] mb-4 ">
                   <svg
@@ -243,11 +240,11 @@ const ContactForm: React.FC<ContactFormProps> = ({ data }) => {
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
-                    stroke-width="1"
+                    strokeWidth="1"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                       d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
                     />
                   </svg>
