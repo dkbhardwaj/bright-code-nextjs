60e5a1d8b062b101e4ed5eb37fc30407d958d765
Devinder Bhardwaj <dk@webenertia.com>
2024-05-15 23:07:44 -0700

    Test firebase function

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,10 @@
-// Listens for new messages added to /messages/:documentId/original
-// and saves an uppercased version of the message
-// to /messages/:documentId/uppercase
-exports.makeuppercase = onDocumentCreated("/messages/{documentId}", (event) => {
-    // Grab the current value of what was written to Firestore.
-    const original = event.data.data().original;
-  
-    // Access the parameter `{documentId}` with `event.params`
-    logger.log("Uppercasing", event.params.documentId, original);
-  
-    const uppercase = original.toUpperCase();
-  
-    // You must return a Promise when performing
-    // asynchronous tasks inside a function
-    // such as writing to Firestore.
-    // Setting an 'uppercase' field in Firestore document returns a Promise.
-    return event.data.ref.set({uppercase}, {merge: true});
-  });
+// The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
+const {logger} = require("firebase-functions");
+const {onRequest} = require("firebase-functions/v2/https");
+const {onDocumentCreated} = require("firebase-functions/v2/firestore");
+
+// The Firebase Admin SDK to access Firestore.
+const {initializeApp} = require("firebase-admin/app");
+const {getFirestore} = require("firebase-admin/firestore");
+
+initializeApp();
