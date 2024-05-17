b1399e6a24a2664f97990c5b38e3a83db7a66ce0
Roovin <roovin@cleardigital.com>
2024-05-17 18:02:52 +0530

    optimize image code

--- next.config.js
+++ next.config.js
@@ -4,7 +4,10 @@
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
-    /* config options here */
+  images: {
+    unoptimized: false,
+  },
+  reactStrictMode: true,
   }
    
   module.exports = nextConfig
