c9bf8e46722fbeaf5f855610aad31e5a70410c0b
Bhawana <bhawana@webenertia.com>
2024-05-17 17:08:02 +0530

    update Herobanner

--- components/Herobanner.tsx
+++ components/Herobanner.tsx
@@ -24,7 +24,8 @@ const Herobanner: React.FC<HeroBannerData> = ({ data }) => {
             src={bannerRightImg}
             width={550}
             height={550}
-            loading="lazy"
+            // loading="lazy"
+            loading="eager"
             alt="right-img"
             className=" w-full h-full object-cover"
           />
@@ -36,7 +37,8 @@ const Herobanner: React.FC<HeroBannerData> = ({ data }) => {
             src={bannerLeftImg}
             width={550}
             height={550}
-            loading="lazy"
+            // loading="lazy"
+            loading="eager"
             alt="left-img"
             className=" w-full h-full object-cover"
           />
