5dd4829b75751380c721ff2069dc3dc86f86f6ba
Bhawana <bhawana@webenertia.com>
2024-05-20 14:54:00 +0530

    update document.js

--- components/ScrollToTopButton.tsx
+++ components/ScrollToTopButton.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function ScrollToTopButton() {
   const [isVisible, setIsVisible] = useState(false);
