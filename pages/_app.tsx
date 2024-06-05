35bac2262fecb7210adcb17f361154148167222c
Devinder Bhardwaj <dk@webenertia.com>
2024-06-04 20:15:31 -0700

    Testing page speed 4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,37 +1,8 @@
-import type { ReactElement, ReactNode } from "react";
-import { Metadata } from "next";
-import type { NextPage } from "next";
-import type { AppProps } from "next/app";
-import "../app/globals.css";
-import "../styles/global.scss";
-import "../styles/buttons.scss";
-import "../styles/banners.scss";
-
-
-
-
-
-
-export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode;
-};
-
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
-};
-
-export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+export default function MyApp() {
  
-
-
   return (
-    <>
-     
-      
-        <main>
+     <main>
          <h1>Homepage</h1>
         </main>
-    
-    </>
   );
 }
