3c381a36b72418bb046db363e2cb8fead7a8863a
Bhawana <bhawana@webenertia.com>
2024-06-07 18:48:15 +0530

    update app for cache-control

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -39,6 +39,17 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
 
+export async function GET() {
+  return new Response("Cache Control example", {
+    status: 200,
+    headers: {
+      "Cache-Control": "public, s-maxage=1",
+      "CDN-Cache-Control": "public, s-maxage=60",
+      "Vercel-CDN-Cache-Control": "public, s-maxage=3600",
+    },
+  });
+}
+
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   useEffect(() => {
     var anchors = document.querySelectorAll("a");
