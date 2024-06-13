af41aa43b7e9a3b479c5648da467542469acaa69
Bhawana <bhawana@webenertia.com>
2024-06-13 16:51:35 +0530

    update Herobanner bg-image

--- components/Herobanner.tsx
+++ components/Herobanner.tsx
@@ -16,24 +16,18 @@ const Herobanner: React.FC<HeroBannerData> = ({ data }) => {
     data;
   return (
     <section
-      className={`banner-with-img banner_overlay relative bg-[url("/banner-bg-img.svg")] bg-no-repeat bg-cover  overflow-x-hidden bg-darkBlue flex items-end text-white z-[1] pt-28 pb-24 min-h-[752px] desktop:min-h-[630px]  tablet:min-h-[560px] tablet:pt-24 tablet:pb-14 md:min-h-[552px]  md:pt-24 md:pb-12 md:items-center `}
+      className={`banner-with-img banner_overlay relative  overflow-x-hidden bg-darkBlue flex items-end text-white z-[1] pt-28 pb-24 min-h-[752px] desktop:min-h-[630px]  tablet:min-h-[560px] tablet:pt-24 tablet:pb-14 md:min-h-[552px]  md:pt-24 md:pb-12 md:items-center `}
     >
-      {/* {bannerLeftImg && (
-        <div className="left_img absolute bottom-0 right-[61%] w-full max-w-[526px] h-[506px] z-[1] desktop:max-w-[450px] desktop:h-[450px]  tablet:max-w-[400px] tablet:h-[400px] xl:right-[65%] md:h-[310px] md:max-w-[325px] md:!right-[35%] ">
-          <Image
-            src={bannerLeftImg}
-            // width={550}
-            // height={550}
-            // loading="lazy"
-            alt="left-img"
-            className=" w-full h-full object-cover"
-            priority
-            layout="fill" // Fills container while maintaining aspect ratio
-            objectFit="cover"
-            quality={80}
-          />
-        </div>
-      )} */}
+      <div className="bg_image absolute top-0 left-0 w-full h-full ">
+        <Image
+          src="/banner-bg-img.svg"
+          width={2500}
+          height={900}
+          alt="bg-img"
+          className=" w-full h-full object-cover"
+          loading="lazy"
+        />
+      </div>
       <div className="container">
         {bannerLeftImg && (
           <div className="left_img absolute bottom-0 right-[60%] w-full max-w-[526px] h-[506px] z-[1] desktop:max-w-[450px] desktop:h-[450px]  tablet:max-w-[400px] tablet:h-[400px] xl:right-auto tablet:left-0 md:h-[310px] md:max-w-[325px]  md:left-[50%] md:translate-x-[-50%] ">
