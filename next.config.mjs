c818683cfa18f2154219ec48e06d072b4048f8a9
Anoop <anoop@cleardigital.com>
2024-09-24 12:51:33 +0530

    change redirection of ourclients and whychooseus

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,20 @@ const nextConfig = {
   images: {
     domains: ['images.ctfassets.net'], // Add the hostname here
   },
+  async redirects() {
+    return [
+      {
+        source: '/whychooseus',
+        destination: '/why-choose-us',
+        permanent: true,
+      },
+      {
+        source: '/ourclients',
+        destination: '/our-clients',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
