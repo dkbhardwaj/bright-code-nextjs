fc571d4ecfcedee7efa763bc1cb55dfcdcbeb529
Anoop <anoop@cleardigital.com>
2025-01-20 22:59:09 +0530

    update sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,59 @@
+export default async function sitemap() {
+    return [
+        {
+            url: 'https://www.bright-code.io/',
+            lastModified: new Date('2024-04-14T05:35:02+00:00'),
+            priority: 1.00
+        },
+        {
+            url: 'https://www.bright-code.io/whychooseus',
+            lastModified: new Date('2024-04-14T05:35:02+00:00'),
+            priority: 0.80
+        },
+        {
+            url: 'https://www.bright-code.io/white-label-development',
+            lastModified: new Date('2024-04-14T05:35:02+00:00'),
+            priority: 0.80
+        },
+        {
+            url: 'https://www.bright-code.io/ourclients',
+            lastModified: new Date('2024-04-14T05:35:02+00:00'),
+            priority: 0.80
+        },
+        {
+            url: 'https://www.bright-code.io/contact',
+            lastModified: new Date('2024-04-14T05:35:02+00:00'),
+            priority: 0.80
+        },
+        {
+            url: 'https://www.bright-code.io/dedicated-team',
+            lastModified: new Date('2024-04-14T05:35:02+00:00'),
+            priority: 0.80
+        },
+        {
+            url: 'https://www.bright-code.io/cms-implementation',
+            lastModified: new Date('2024-04-14T05:35:02+00:00'),
+            priority: 0.80
+        },
+        {
+            url: 'https://www.bright-code.io/cms-support',
+            lastModified: new Date('2024-04-14T05:35:02+00:00'),
+            priority: 0.80
+        },
+        {
+            url: 'https://www.bright-code.io/privacy-policy',
+            lastModified: new Date('2024-04-14T05:35:02+00:00'),
+            priority: 0.80
+        },
+        {
+            url: 'https://www.bright-code.io/tools/security-header',
+            lastModified: new Date('2024-04-14T05:35:02+00:00'),
+            priority: 0.80
+        },
+        {
+            url: 'https://www.bright-code.io/tools/link-tracker',
+            lastModified: new Date('2024-04-14T05:35:02+00:00'),
+            priority: 0.80
+        }
+    ]
+}
