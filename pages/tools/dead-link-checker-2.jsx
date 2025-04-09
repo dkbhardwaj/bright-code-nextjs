1193c0ae554715a6ea85adb186870f34b386e3a4
Ganesh <ganesh@cleardigital.com>
2025-04-09 19:05:07 +0530

    update 404 tool layout

--- pages/tools/dead-link-checker-2.jsx
+++ pages/tools/dead-link-checker-2.jsx
@@ -94,25 +94,45 @@ export default function DeadLinkChecker() {
       setIsLoading(false);
     }
   };
+  console.log(progress);
+  console.log(isLoading);
 
   return (
-    <section className="section_bgImage bg-darkBlue min-h-screen bg-gray-100 flex flex-col items-center justify-center">
-      <div className="w-[calc(100%-40px)] max-w-4xl p-8 bg-white shadow-lg rounded-lg m-[20px] z-[1]">
-        <h1 className={styles.title}>404 Link Checker</h1>
-        <p className={styles.description}>Find broken links on your website</p>
+    <>
+      {progress != 100 && (
+        <section className="section_bgImage bg-darkBlue min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+          <div className="w-[calc(100%-40px)] max-w-4xl p-8 bg-white shadow-lg rounded-lg m-[20px] z-[1]">
+            <h1 className={styles.title}>404 Link Checker</h1>
+            <p className={styles.description}>
+              Find broken links on your website
+            </p>
 
-        <LinkCheckerForm
-          onSubmit={handleCheckLinks}
-          isLoading={isLoading}
-          progress={progress}
-        />
+            <LinkCheckerForm
+              onSubmit={handleCheckLinks}
+              isLoading={isLoading}
+              progress={progress}
+            />
+          </div>
+        </section>
+      )}
+      {results.length == 0 && progress == 100 ? (
+        <section className="section_bgImage bg-darkBlue min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+          <div className="w-[calc(100%-40px)] max-w-4xl p-8 bg-white shadow-lg rounded-lg m-[20px] z-[1]">
+            <h1 className={styles.title}>404 Link Checker</h1>
 
-        {results.length == 0 && progress == 100 ? (
-          <h3>No broken link found</h3>
-        ) : (
-          <ResultsTable results={results} isLoading={isLoading} />
-        )}
-      </div>
-    </section>
+            <h3>No broken link found</h3>
+          </div>
+        </section>
+      ) : (
+        <section className="section_bgImage bg-darkBlue min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+          <div className="w-[calc(100%-40px)] max-w-4xl p-8 bg-white shadow-lg rounded-lg m-[20px] z-[1]">
+            <h1 className="text-2xl font-bold text-darkBlue text-center mb-6">
+              404 Link Checker
+            </h1>
+            <ResultsTable results={results} isLoading={isLoading} />
+          </div>
+        </section>
+      )}
+    </>
   );
 }
