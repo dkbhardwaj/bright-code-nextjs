8a6703cb269fe92d85052250679b98f34c26d190
Anoop <anoop@cleardigital.com>
2025-05-20 19:54:41 +0530

    udpate

--- components/footer.tsx
+++ components/footer.tsx
@@ -98,35 +98,35 @@ const Footer: React.FC = () => {
             </ul> */}
           </div>
           <div className="textWrap relative w-full max-w-[400px] md:mt-8   md:mx-auto">
-            <ul className=" relative flex flex-wrap justify-end sm:block">
-              <li className=" relative w-fit flex items-center px-5 py-2 border-[1px] border-solid border-white rounded-[9px]  mb-5 sm:mb-3 md:mx-auto ">
+            <ul className=" relative flex flex-wrap  justify-end sm:block">
+              <li className=" relative w-full justify-end flex items-center   rounded-[9px]  mb-5 sm:mb-3 md:mx-auto ">
                 <Link href="mailto:contact@bright-code.io" className="redirect">
                   .
                 </Link>
                 <div className="icon-img max-w-[21px] h-[21px] mr-5">
-                  <Image
+                  {/* <Image
                     src="/mail-icon-white.svg"
                     width={25}
                     height={25}
                     loading="lazy"
                     alt="img"
                     className=" w-full h-full object-contain"
-                  />
+                  /> */}
                 </div>
                 <span className="font-semibold">contact@bright-code.io</span>
               </li>
-              <li className=" relative w-fit flex items-center  px-5 py-2 border-[1px] border-solid border-white rounded-[9px] ml-4  mb-5 sm:mb-3 md:mx-auto ">
+              <li className=" relative w-full justify-end flex items-center  rounded-[9px] ml-4  mb-5 sm:mb-3 md:mx-auto ">
                 <div className="icon-img max-w-[21px] h-[21px] mr-5">
-                  <Image
+                  {/* <Image
                     src="/phone-icon-white.svg"
                     width={25}
                     height={25}
                     loading="lazy"
                     alt="img"
                     className=" w-full h-full object-contain"
-                  />
+                  /> */}
                 </div>
-                <span className="font-semibold">+ ‪805-215-0549‬</span>
+                <span className="font-semibold">‪805-215-0549‬</span>
               </li>
             </ul>
             <div className="socialWrap relative w-full flex justify-end mt-12 md:justify-center md:hidden">
@@ -204,8 +204,8 @@ const Footer: React.FC = () => {
           </div>
         </div>
         <div className=" relative w-full mt-16 flex justify-between items-center flex-wrap md:mt-10">
-          <div className="copyright w-full flex justify-center sm:block sm:text-center ">
-            <p className="text-white">© {year} Bright Code Technologies LLC</p>
+          <div className="copyright w-full flex justify-end sm:block sm:text-center ">
+            <p className="text-white">© {year} Bright Code Technologies Inc</p>
             <ul className=" relative w-fit flex sm:w-full sm:justify-center ">
               <li className=" ml-4 pl-4 relative before:content-[''] before:absolute before:top-[7px] before:left-0 before:w-[1px] before:h-[18px] before:bg-white md:ml-3 md:pl-3 md:before:top-[9px] md:before:h-[15px] sm:before:hidden sm:!ml-0 sm:!pl-0 ">
                 <Link
