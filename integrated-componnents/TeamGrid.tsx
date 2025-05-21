b32808f33d5b674c2054e74e3a26f4e43f2e74ee
Anoop <anoop@cleardigital.com>
2025-05-21 20:12:52 +0530

    fix build

--- integrated-componnents/TeamGrid.tsx
+++ integrated-componnents/TeamGrid.tsx
@@ -1,46 +1,52 @@
-'use client'
-import Image from "next/image"
-import React from "react"
+'use client';
+import Image from "next/image";
+import React from "react";
+
+interface TeamMemberCard {
+    fields: {
+        memberImage: {
+            fields: {
+                file: {
+                    url: string;
+                };
+                title: string;
+            };
+        };
+        subTitle: string;
+        title: string;
+    };
+}
 
 interface CardItem {
     data: {
         title: string;
-        sectionPadding: any;
-        card:{
-            fields :{
-                memberImage:{
-                    fields:{
-                        file:{
-                            url:string;
-                        }
-                        title:string;
-                    }
-                },
-                subTitle:string;
-                title:string;
+        sectionPadding: {
+            fields: {
+                padding: string[];
             };
         };
-      };
+        card: TeamMemberCard[];
+    };
 }
 
 const TeamGrid: React.FC<CardItem> = ({ data }) => {
-
     const {
         title,
         sectionPadding,
         card
-      } = data;
+    } = data;
 
-      const padding = (data?.sectionPadding?.fields?.padding)?.join(" ")
+    const padding = sectionPadding?.fields?.padding?.join(" ") || "";
 
     return (
         <section className={`team-grid relative ${padding} no-padding-bottom z-[1]`}>
             <div className="container">
-                {/* <div className="intro mb-[4rem]">
-                </div> */}
                 <div className="wrapper flex w-[calc(100%+2rem)] justify-center flex-wrap ml-[-1rem]">
                     {card.map((item, index) => (
-                        <div className="group sm:max-w-[33rem] w-[calc(33.33%-2rem)] sm:!w-full lg:w-[calc(50%-3rem)] mx-[1rem] mb-[2.5rem]" key={index}>
+                        <div
+                            className="group sm:max-w-[33rem] w-[calc(33.33%-2rem)] sm:!w-full lg:w-[calc(50%-3rem)] mx-[1rem] mb-[2.5rem]"
+                            key={index}
+                        >
                             <div className="img-wrap grayscale group-hover:grayscale-0 md:grayscale-0 duration-300 sm:max-w-[33rem] h-[25rem] mb-[1.5rem]">
                                 <Image
                                     src={`https:${item.fields.memberImage.fields.file.url}`}
@@ -51,14 +57,18 @@ const TeamGrid: React.FC<CardItem> = ({ data }) => {
                                     alt={`${item.fields.memberImage.fields.title}'s portrait`}
                                 />
                             </div>
-                            <h4 className="subtitle text-black !font-bold mb-[0.4rem]">{item.fields.title}</h4>
-                            <p className="small text-black font-normals">{item.fields.subTitle}</p>
+                            <h4 className="subtitle text-black !font-bold mb-[0.4rem]">
+                                {item.fields.title}
+                            </h4>
+                            <p className="small text-black font-normals">
+                                {item.fields.subTitle}
+                            </p>
                         </div>
                     ))}
                 </div>
             </div>
         </section>
     );
-}
+};
 
 export default TeamGrid;
