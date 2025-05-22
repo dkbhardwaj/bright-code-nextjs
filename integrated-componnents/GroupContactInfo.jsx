aa85e5eb0832d204dde2ea220954ab08509d935a
Anoop <anoop@cleardigital.com>
2025-05-22 15:07:55 +0530

    udpate

--- integrated-componnents/GroupContactInfo.jsx
+++ integrated-componnents/GroupContactInfo.jsx
@@ -10,7 +10,7 @@ export default function GroupContactInfo({data}) {
   let bgImg = data?.backgroundImage?.fields?.file?.url
 
   return (
-    <div className={`relative ${bgImg ? "section_overlay_dark" : "section_bgImage bg-darkBlue"} ${padding}`}>
+    <div className={`relative md:overflow-hidden ${bgImg ? "section_overlay_dark" : "section_bgImage bg-darkBlue"} ${padding}`}>
       {bgImg && (
               <div className="absolute top-0 left-0 w-full h-full ">
                 <Image
