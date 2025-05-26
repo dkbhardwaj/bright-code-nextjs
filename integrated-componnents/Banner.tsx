7af7aaed75b25779454a4a7c791bc442dbdc0f76
Anoop <anoop@cleardigital.com>
2025-05-26 16:36:18 +0530

    update

--- integrated-componnents/Banner.tsx
+++ integrated-componnents/Banner.tsx
@@ -17,19 +17,28 @@ interface BannerData {
         };
       };
     };
+    backgroundVideo: {
+      fields:{
+        file:{
+          url: string;
+          fileName:string
+        }
+      }
+    };
   };
 }
 
 const Banner: React.FC<BannerData> = ({ data }) => {
-  const { subTitle, title, backgroundImage, description,eyebrowText,alignTextCenter } = data;
+  const { subTitle, title, backgroundImage, description,eyebrowText,alignTextCenter, backgroundVideo} = data;
   //banner_overlayImg
-  
+    console.log(data)
   return (
     <section
-      className={`banner banner_overlayImg relative overflow-x-hidden bg-darkBlue flex items-center text-white pt-28 pb-10 min-h-[752px]  desktop:min-h-[630px]  tablet:min-h-[560px] tablet:pt-24 md:min-h-[552px]  md:pt-24  md:items-center `}
+      className={`banner ${backgroundVideo?.fields?.file?.url ? "" : "banner_overlayImg"}  relative overflow-x-hidden bg-darkBlue flex items-center text-white pt-28 pb-10 min-h-[752px]  desktop:min-h-[630px]  tablet:min-h-[560px] tablet:pt-24 md:min-h-[552px]  md:pt-24  md:items-center `}
     >
-      <div className="bannerBgImage absolute top-0 left-0 w-full h-full  ">
+     
         {backgroundImage?.fields?.file?.url && 
+         <div className="bannerBgImage absolute top-0 left-0 w-full h-full  ">
             <Image
             src={`https:${backgroundImage?.fields?.file?.url}`}
             width={2500}
@@ -38,9 +47,29 @@ const Banner: React.FC<BannerData> = ({ data }) => {
             className=" w-full h-full object-cover object-center "
             loading="lazy"
           />
+           </div>
         }
+
+{backgroundVideo && ( <div className="absolute left-0 right-0 bottom-0 h-full bg-black opacity-[0.6] z-[2]"></div>)}
+      {backgroundVideo && (
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="banner-video object-cover absolute left-0 top-0 w-full h-full "
+          width={1000}
+          height={964}
+        >
+          <source
+            src={`https:${backgroundVideo?.fields?.file?.url}`}
+            type="video/mp4"
+          />
+          Your browser does not support the video tag.
+        </video>
+      )}
         
-      </div>
+     
       <div className="container">
         <div
           className={`content relative z-[10] w-full relative z-[5] ${alignTextCenter ? "text-center" : "text-left"}`}
