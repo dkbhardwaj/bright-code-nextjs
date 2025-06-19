83ff6bface9ca34c444ab5edaeaa90fab71a2195
Anoop <anoop@cleardigital.com>
2025-06-19 12:40:32 +0530

    add script before closing body

--- pages/_document.js
+++ pages/_document.js
@@ -74,6 +74,24 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <Script type="text/javascript" id="linkedin-insight-init" strategy="afterInteractive" dangerouslySetInnerHTML={{
+            __html: `_linkedin_partner_id = "7372028";
+            window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+            window._linkedin_data_partner_ids.push(_linkedin_partner_id);`
+          }} />
+          <Script type="text/javascript" id="linkedin-insight-loader" strategy="afterInteractive" dangerouslySetInnerHTML={{
+                    __html: `(function(l) {
+                  if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+                  window.lintrk.q=[]}
+                  var s = document.getElementsByTagName("script")[0];
+                  var b = document.createElement("script");
+                  b.type = "text/javascript";b.async = true;
+                  b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+                  s.parentNode.insertBefore(b, s);})(window.lintrk);`
+                  }} />
+          <noscript>
+            <img height="1" width="1" style={{display: 'none'}} alt="" src="https://px.ads.linkedin.com/collect/?pid=7372028&fmt=gif" />
+          </noscript>
         </body>
       </Html>
     );
