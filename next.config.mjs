d096e9ec842e523da37ff70bb73927576f786ae9
Anoop <anoop@cleardigital.com>
2025-07-23 13:59:58 +0530

    fix issue

--- next.config.mjs
+++ next.config.mjs
@@ -24,24 +24,20 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://snap.licdn.com; style-src 'self' 'unsafe-inline' https://unpkg.com; img-src 'self' data: https://px.ads.linkedin.com; connect-src 'self'; frame-src 'self';"
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://snap.licdn.com https://googleads.g.doubleclick.net https://www.googleadservices.com https://www.google.com https://www.gstatic.com;
+              style-src 'self' 'unsafe-inline' https://unpkg.com;
+              img-src 'self' data: https://px.ads.linkedin.com https://px4.ads.linkedin.com https://images.ctfassets.net https://www.google-analytics.com https://www.google.com https://www.google.co.in https://www.googleadservices.com;
+              media-src 'self' https://videos.ctfassets.net;
+              connect-src 'self' https://www.google.com https://www.googletagmanager.com https://www.google-analytics.com https://*.ctfassets.net https://cdn.contentful.com https://px.ads.linkedin.com https://px4.ads.linkedin.com;
+              frame-src 'self' https://www.googletagmanager.com https://td.doubleclick.net https://www.google.com;
+            `.replace(/\s{2,}/g, ' ').trim()
           },
-          {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN'
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff'
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'strict-origin-when-cross-origin'
-          },
-          {
-            key: 'Permissions-Policy',
-            value: 'camera=(), microphone=(), geolocation=()'
-          }
+          { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
+          { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=()' }
         ]
       }
     ];
