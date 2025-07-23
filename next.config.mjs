b8a9d7abda9b6d777a7c9b122f5749534ab8cfd0
Anoop <anoop@cleardigital.com>
2025-07-23 14:49:12 +0530

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -28,9 +28,9 @@ const nextConfig = {
               default-src 'self';
               script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://snap.licdn.com https://googleads.g.doubleclick.net https://www.googleadservices.com https://www.google.com https://www.gstatic.com;
               style-src 'self' 'unsafe-inline' https://unpkg.com;
-              img-src 'self' data: https://px.ads.linkedin.com https://px4.ads.linkedin.com https://images.ctfassets.net https://www.google-analytics.com https://www.google.com https://www.google.co.in https://www.googleadservices.com;
+              img-src 'self' data: https://px.ads.linkedin.com https://px4.ads.linkedin.com https://images.ctfassets.net https://www.google-analytics.com https://www.google.com https://www.google.co.in https://www.googleadservices.com https://googleads.g.doubleclick.net;
               media-src 'self' https://videos.ctfassets.net;
-              connect-src 'self' https://www.google.com https://www.googletagmanager.com https://www.google-analytics.com https://*.ctfassets.net https://cdn.contentful.com https://px.ads.linkedin.com https://px4.ads.linkedin.com;
+              connect-src 'self' https://google.com https://www.google.com https://*.google.com https://www.googletagmanager.com https://www.google-analytics.com https://*.ctfassets.net https://cdn.contentful.com https://px.ads.linkedin.com https://px4.ads.linkedin.com https://*.google-analytics.com https://*.google.co.in https://*.googleadservices.com;
               frame-src 'self' https://www.googletagmanager.com https://td.doubleclick.net https://www.google.com;
             `.replace(/\s{2,}/g, ' ').trim()
           },
