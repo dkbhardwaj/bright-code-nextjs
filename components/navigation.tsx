b541120bfaec70f8dc9d0bbd8bdece1d1a083686
Anoop <anoop@cleardigital.com>
2025-08-05 18:12:29 +0530

    not fix try another way

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -231,7 +231,7 @@ const Navigation: React.FC<NavigationProps> = ({ navigationData }: NavProps) => 
                               className={`subMenu absolute lg:relative min-w-[160px] w-fit top-[50px] lg:top-0 left-0 lg:px-[10px] lg:py-0 rounded-[2px] bg-white lg:overflow-hidden lg-up:overflow-hidden lg:transition-all lg:duration-300 ${
                                 linkValue === "Try Our Tools"
                                   ? "lg-up:opacity-1 lg-up:max-h-[500px] lg-up:px-[20px] lg-up:py-[15px] lg:max-h-[500px] lg:py-[10px]"
-                                  : "lg-up:opacity-0 lg-up:max-h-0 lg-up:p-0 lg:max-h-0 "
+                                  : ""
                               }`}
                               role="menu" aria-label="Try Our Tools Menu"
                             >
