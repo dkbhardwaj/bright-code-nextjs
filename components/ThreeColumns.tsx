1c0918eeff5815bb64fc8d1d4c589fe5b0bed70e
Atiksha <atiksha@cleardigital.com>
2025-12-16 20:44:21 +0530

    Added the hover on cards.

---
+++ components/ThreeColumns.tsx
@@ -0,0 +1,97 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+interface ThreeColumnCard {
+  id: number;
+  cardDelay?: string;
+  cardDuration?: string;
+  imageUrl: string;
+  imageAlt: string;
+  cardTitle: string;
+  cardDetail: string;
+    link: string;
+    linkText: string;
+}
+
+interface ThreeColumnsData {
+  paddingLarge?: boolean;
+  featuredClass?: boolean;
+  title?: string;
+  bgTransparent?: boolean;
+  threeCards: ThreeColumnCard[];
+  btntext?: string;
+  btnUrl?: string;
+}
+
+interface ThreeColumnsProps {
+  data: ThreeColumnsData;
+}
+
+const ThreeColumns: React.FC<ThreeColumnsProps> = ({ data }) => {
+  const { paddingLarge, title, threeCards, btntext, btnUrl } = data;
+
+  return (
+    <section className={`ThreeColumns padding-large-bottom`}>
+      <div className="container">
+        <div className="w-[calc(100%+24px)] ml-[-12px] flex flex-wrap justify-center">
+          {threeCards.map((card) => (
+            <div
+              key={card.id}
+              className="colThree relative w-[calc(33.3%-24px)] tablet:w-[calc(50%-24px)] phablet:w-[calc(50%-24px)] sm:w-[calc(100%-24px)] mx-[12px] mb-[24px]"
+            >
+              <Link href={card.link} className="redirect rounded-[24px] overflow-hidden">
+                {card.linkText}
+              </Link>
+              <div className="card flex flex-col justify-between bg-white text-center rounded-[24px] overflow-hidden py-[40px] px-[30px] border border-[#E5E5EA] h-full">
+                <div className="content">
+                  {/* Image */}
+                  <div className="imageWrap max-w-[350px] h-[200px] mx-auto mb-[25px]">
+                    <Image
+                      src={card.imageUrl}
+                      width={400}
+                      height={300}
+                      alt={card.imageAlt}
+                      className="w-full h-full object-contain"
+                    />
+                  </div>
+                  {/* Title */}
+                  <h4 className="font-[600] text-[20px] md:text-[16px]">
+                    {card.cardTitle}
+                  </h4>
+
+                  {/* Description */}
+                  <p className="text-[16px] md:text-[12px] mt-[5px] text-[#6b7280]">
+                    {card.cardDetail}
+                  </p>
+                </div>
+                {/* CTA */}
+                {card.linkText && card.link && (
+                  <div className="btn-wrap">
+                    <Link
+                      href={card.link}
+                      className="link inline-block text-[#0044FF] text-[12px] mt-[10px]"
+                    >
+                      Learn more
+                    </Link>
+                  </div>
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
+
+        {/* Bottom Button */}
+        {btntext && btnUrl && (
+          <div className="text-center mt-[40px]">
+            <Link href={btnUrl} className="gradient-btn-blue">
+              {btntext}
+            </Link>
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default ThreeColumns;
--- pages/demo.tsx
+++ pages/demo.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ColTwoCard from "../components/ColTwoCard";
 import TitleSection from "../components/TitleSection";
+import ThreeColumns from "../components/ThreeColumns";
+import { link } from "fs";
 
 const Demo: React.FC = () => {
   const darkMode = false;
@@ -34,6 +36,62 @@ const Demo: React.FC = () => {
     buttonText: "Learn more",
     buttonLink: "/services",
   };
+  const titleSectionData2 = {
+    badgeText: "About Bright Code",
+    title: "Who we are here to help",
+    description:
+      "Bright Code works seamlessly with your team to help you envision and create your clients’ digital presence. Whether it’s building something from the ground up or developing and QA-ing existing architecture, we create sophisticated websites and content management systems.",
+    buttonText: "",
+    buttonLink: "",
+  };
+
+const colThreeCardsData = {
+  paddingLarge: true,
+  featuredClass: false,
+  title: "Our Services",
+  bgTransparent: false,
+  threeCards: [
+    {
+      id: 1,
+      cardDelay: "0.1s",
+      cardDuration: "0.6s",
+      imageUrl: "/what-we-do-blades/channels.png",
+      imageAlt: "card_1",
+      cardTitle: "Creative and Brand Agencies",
+      cardDetail:
+        "We have over a decade of experience using leading technologies to provide support.",
+      link: "#",
+      linkText: "Learn more",
+    },
+    {
+      id: 2,
+      cardDelay: "0.1s",
+      cardDuration: "0.6s",
+      imageUrl: "/what-we-do-blades/team.png",
+      imageAlt: "card_1",
+      cardTitle: "Marketing and Digital Teams",
+      cardDetail:
+        "We have over a decade of experience using leading technologies to provide support.",
+      link: "#",
+      linkText: "Learn more",
+    },
+    {
+      id: 3,
+      cardDelay: "0.1s",
+      cardDuration: "0.6s",
+      imageUrl: "/what-we-do-blades/tools.png",
+      imageAlt: "card_1",
+      cardTitle: "Teams with too much on their Plate",
+      cardDetail:
+        "We have over a decade of experience using leading technologies to provide support.",
+      link: "#",
+      linkText: "Learn more",
+    },
+  ],
+  btntext: "Learn more",
+  btnUrl: "#",
+};
+
 
   return (
     <>
@@ -42,6 +100,8 @@ const Demo: React.FC = () => {
         <ColTwoCard {...cardData} />
         <ColTwoCard {...cardData2} />
         <TitleSection {...titleSectionData} />
+        <TitleSection {...titleSectionData2} />
+        <ThreeColumns data={colThreeCardsData} />
       </div>
     </>
   );
