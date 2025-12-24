2819a639a45d80b44cf2a52da474f49f6fb51dd6
Atiksha <atiksha@cleardigital.com>
2025-12-24 22:00:32 +0530

    Fixed the build.

--- components/layout.tsx
+++ components/layout.tsx
@@ -10,7 +10,7 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children, navigationData }) => {
   return (
     <>
-      <Navigation navigationData={navigationData} />
+      <Navigation />
       {children}
       <Footer />
     </>
